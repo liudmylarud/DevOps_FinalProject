0a870232ea6fa4f8a16d0a80e2d869932577e0fe
Liudmyla Rud <cherry.pie@ukr.net>
2020-11-09 20:29:28 +0200

    Commit 2

--- Kotiko/Kotiko/Startup.cs
+++ Kotiko/Kotiko/Startup.cs
@@ -24,10 +24,6 @@ namespace Kotiko
             .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
             .AddEnvironmentVariables();
 
-            if (env.IsDevelopment())
-            {
-                builder.AddUserSecrets<Startup>();
-            }
 
             Configuration = builder.Build();
         }
